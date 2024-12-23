fc75690d47be9dd8b0aa94e7bfca9299dad7f012
Owen Kephart <owenkephart@gmail.com>
2024-12-23 12:08:42 -0500

    [core-apis][experimental] observable_source_asset, multi_observable_source_asset

--- python_modules/dagster/dagster/_core/definitions/decorators/source_asset_decorator.py
+++ python_modules/dagster/dagster/_core/definitions/decorators/source_asset_decorator.py
@@ -2,7 +2,7 @@ from collections.abc import Mapping, Sequence
 from typing import AbstractSet, Any, Callable, Optional, Union, overload  # noqa: UP035
 
 import dagster._check as check
-from dagster._annotations import experimental, hidden_param
+from dagster._annotations import hidden_param
 from dagster._core.definitions.asset_check_spec import AssetCheckSpec
 from dagster._core.definitions.asset_spec import AssetExecutionType, AssetSpec
 from dagster._core.definitions.assets import AssetsDefinition
@@ -64,7 +64,6 @@ def observable_source_asset(
     breaking_version="1.10.0",
     additional_warn_text="use freshness checks instead.",
 )
-@experimental
 def observable_source_asset(
     observe_fn: Optional[SourceAssetObserveFunction] = None,
     *,
@@ -225,7 +224,6 @@ class _ObservableSourceAsset:
             )
 
 
-@experimental
 def multi_observable_source_asset(
     *,
     specs: Sequence[AssetSpec],
